555fa865334a90e4132313a22b5643be01d5285b
Braun Alexander Maximilian Ralph <alexander.braun@itsincom.it>
2026-04-09 15:49:21 +0200

    Implementate funzioni homepage

--- javascript/index.js
+++ javascript/index.js
@@ -1,15 +1,12 @@
 function apriDocumenti() {
     window.open("documenti.html", "_blank");
 }
-
 function apriFogli() {
     window.open("fogli.html", "_blank");
 }
-
 function apriPresentazioni() {
     window.open("presentazioni.html", "_blank");
 }
-
 function apriCalcolatrice() {
     window.open("calcolatrice.html", "_blank");
 }
